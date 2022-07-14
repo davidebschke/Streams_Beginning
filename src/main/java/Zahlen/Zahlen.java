791d7cb31bd6fdc2445890bdb68934a084c5f5b8
davidebschke <Davidebschke8@gmail.com>
2022-07-14 14:31:59 +0200

    Entfernen von Leeren Zeilen Einrücken der Objekte

--- src/main/java/Zahlen/Zahlen.java
+++ src/main/java/Zahlen/Zahlen.java
@@ -8,33 +8,25 @@ import java.util.stream.Collectors;
 public class Zahlen {
 
 
-
-
-
-
-
-
-
-    public static List<Integer> sortNumbers(){
+    public static List<Integer> sortNumbers() {
         List<Integer> zahlen = Arrays.asList(9, 1, 8, 2, 7, 3, 6, 4, 5);
-        List<Integer> sortedList = zahlen.stream()
-                .sorted()
-                .collect(Collectors.toList());
+        List<Integer> sortedList = zahlen.stream().sorted().collect(Collectors.toList());
         System.out.println(sortedList);
         return sortedList;
     }
 
-    public static int summeZahlen(){
+    public static int summeZahlen() {
         List<Integer> zahlen = Arrays.asList(9, 1, 8, 2, 7, 3, 6, 4, 5);
 
-        int summe= zahlen.stream().collect((Collectors.summingInt(Integer::intValue)));
+        int summe = zahlen.stream().collect((Collectors.summingInt(Integer::intValue)));
         System.out.println(summe);
         return summe;
     }
-    public static int produktZahlen(){
-         List<Integer> zahlen = Arrays.asList(9, 1, 8, 2, 7, 3, 6, 4, 5);
 
-         int ergebnis=zahlen.stream().reduce(1,(x,y) ->x*y);
+    public static int produktZahlen() {
+        List<Integer> zahlen = Arrays.asList(9, 1, 8, 2, 7, 3, 6, 4, 5);
+
+        int ergebnis = zahlen.stream().reduce(1, (x, y) -> x * y);
         System.out.println(ergebnis);
         return ergebnis;
     }
